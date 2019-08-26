2ea37447b2030867b7ecb184108809439b3794ff
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-26 16:31:04 -0500

    added styles folder and starting styles for footer/navbar

--- party_planner/src/components/Footer.js
+++ party_planner/src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Segment } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'; 
 
 
 const Footer = () => {
--- party_planner/src/components/NavBar.js
+++ party_planner/src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Menu } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'; 
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
