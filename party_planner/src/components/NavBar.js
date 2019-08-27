b79db52849c2372a423fee3a6f59b521a1ff180b
Tracie T <traciecodes@gmail.com>
2019-08-27 16:38:00 -0500

    Navbar logout button functional. Need to test remove token

--- party_planner/src/components/NavBar.js
+++ party_planner/src/components/NavBar.js
@@ -3,18 +3,17 @@ import { Menu } from 'semantic-ui-react'
 import { NavLink, Link } from 'react-router-dom'
 
 
-const NavBar = (props) => {
+const NavBar = () => {
   return (
     <Menu>
       <Menu.Item header>Party Planner</Menu.Item>
       <NavLink to='/dashboard'>
         <Menu.Item name="My Events"/>
       </NavLink>
-      <Link>
+      <Link to="/login">
         <Menu.Item name="Logout"           
             onClick={()=>{
             localStorage.removeItem("token");
-            props.push("/login")
           }}
         />
       </Link>
