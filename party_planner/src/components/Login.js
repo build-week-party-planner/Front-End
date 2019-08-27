f7b245e304d9e3a4f3bae0cfc2cfc67b07087ab8
Tracie T <traciecodes@gmail.com>
2019-08-27 16:05:56 -0500

    WIP: Logout button on Navbar

--- party_planner/src/components/Login.js
+++ party_planner/src/components/Login.js
@@ -5,74 +5,63 @@ import axios from "axios";
 import { connect } from 'react-redux'
 import { handleSuccessfulLogin } from '../actions'
 import { Button } from 'semantic-ui-react';
-
-
 function Login(props) {
-
-  const {touched} = props
-  const {errors} = props
-
-  return(
-    <Form className="form">
-          <Field
-            hintText="Enter your email"
-            name="email"
-            type="text"
-          />
-          <p>{touched.email && errors.email}</p>
-          <Field
-            hintText="Password"
-            name="password"
-            type="password"
-          />
-          <p>{touched.password && errors.password}</p>
-          <Button>Log In</Button>
-          <br />
-        <input type="checkbox" id="remember_me" name="_remember_me"  method="post"/>
-        <label for="remember_me">Keep me logged in</label>
-
-    </Form>
-  
-  )
+ const {touched} = props
+ const {errors} = props
+ return(
+   <Form className="form">
+         <Field
+           hintText="Enter your email"
+           name="email"
+           type="text"
+         />
+         <p>{touched.email && errors.email}</p>
+         <Field
+           hintText="Password"
+           name="password"
+           type="password"
+         />
+         <p>{touched.password && errors.password}</p>
+         <Button>Log In</Button>
+         <br />
+       <input type="checkbox" id="remember_me" name="_remember_me"  method="post"/>
+       <label for="remember_me">Keep me logged in</label>
+   </Form>
+ )
 }
-
-
 const FormikLogin = withFormik({
-    mapPropsToValues({email, password}) {
-      return {
-        email: email || "",
-        password: password || "",
-        rememberMe: true
-      };
-    },
-    validationSchema: Yup.object().shape({
-      email: Yup.string().required("Username is required"),
-      // password: Yup.string().min(8).required("Password is ALSO required")
-    }),
-  
-    
-    //save token to local storage
-    handleSubmit(values, props) {
-      console.log(props)
-      const propsToSubmit = {"email": values.email, "password": values.password}
-      const url = "https://bw-party-planner.herokuapp.com/api/auth/login";
-      axios
-      .post(url, propsToSubmit)
-        .then(results => {
-          localStorage.setItem("user_id", results.data.id)
-          localStorage.setItem("token", results.data.token);
-          props.props.handleSuccessfulLogin(results.data.id)
-          props.props.history.push(`/dashboard/${results.data.id}`)
-        })
-        .catch(error => {
-          console.log("Error: ", error.response)
-        })
-    }
-  })(Login);
-
-  const mapStateToProps = state => {
-    return{
-      state
-    }
-  }
-  export default connect(mapStateToProps,{handleSuccessfulLogin})(FormikLogin)
+   mapPropsToValues({email, password}) {
+     return {
+       email: email || "",
+       password: password || "",
+       rememberMe: true
+     };
+   },
+   validationSchema: Yup.object().shape({
+     email: Yup.string().required("Username is required"),
+     password: Yup.string().min(8).required("Password is ALSO required")
+   }),
+   //save token to local storage
+   handleSubmit(values, props) {
+     console.log(props)
+     const propsToSubmit = {"email": values.email, "password": values.password}
+     const url = "https://bw-party-planner.herokuapp.com/api/auth/login";
+     axios
+     .post(url, propsToSubmit)
+       .then(results => {
+         localStorage.setItem("user_id", results.data.id);
+         localStorage.setItem("token", results.data.token);
+         props.props.handleSuccessfulLogin(results.data.id);
+         props.props.history.push(`/dashboard/${results.data.id}`);
+       })
+       .catch(error => {
+         console.log("Error: ", error.response)
+       })
+   }
+ })(Login);
+ const mapStateToProps = state => {
+   return{
+     state
+   }
+ }
+ export default connect(mapStateToProps,{handleSuccessfulLogin})(FormikLogin)
--- party_planner/src/components/NavBar.js
+++ party_planner/src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from "react"
 import { Menu } from 'semantic-ui-react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
-const NavBar = () => {
+
+const NavBar = (props) => {
   return (
     <Menu>
       <Menu.Item header>Party Planner</Menu.Item>
       <NavLink to='/dashboard'>
         <Menu.Item name="My Events"/>
       </NavLink>
+      <Link>
+        <Menu.Item name="Logout"           
+            onClick={()=>{
+            localStorage.removeItem("token");
+            props.push("/login")
+          }}
+        />
+      </Link>
     </Menu>
   )
 }
